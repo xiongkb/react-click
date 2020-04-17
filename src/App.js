bf803da9264988e7584600824bf2c5025789800b
xiongkb <mkan-bx@hotmail.com>
2020-04-17 15:24:24 -0700

    added code for images and onclick function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let score = 0;
+  let clicked = [];
+  function clicker(imgNum) {
+    if (!clicked.includes(imgNum)) {
+      score++;
+      clicked.push(imgNum);
+    } else if (clicked.includes(imgNum)) {
+      score = 0;
+      alert("Too bad!")
+    }
+    console.log(clicked)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Clicky Game</h1>
+      <p>Click on the images no more than once! Test your memory skills!</p>
+
+      <div>
+        <img alt="random Img" src="https://picsum.photos/id/237/200/" onClick={() => clicker(1)}></img>
+        <img alt="random Img" src="https://picsum.photos/id/238/200/" onClick={() => clicker(2)}></img>
+        <img alt="random Img" src="https://picsum.photos/id/123/200/" onClick={() => clicker(3)}></img><br></br>
+        <img alt="random Img" src="https://picsum.photos/id/125//200/" onClick={() => clicker(4)}></img>
+        <img alt="random Img" src="https://picsum.photos/id/132//200/" onClick={() => clicker(5)}></img>
+        <img alt="random Img" src="https://picsum.photos/id/144//200/" onClick={() => clicker(6)}></img>
+      </div>
     </div>
   );
 }
