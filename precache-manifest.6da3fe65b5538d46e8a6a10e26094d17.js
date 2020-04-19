8bd8aa78777b52b6a81cbf0a5b114e7430145008
xiongkb <mkan-bx@hotmail.com>
2020-04-19 16:35:15 -0700

    Updates

---
+++ precache-manifest.6da3fe65b5538d46e8a6a10e26094d17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c87c58a069ef69feecf8196c2343db3e",
+    "url": "/react-click/index.html"
+  },
+  {
+    "revision": "bcbeff9d80694e17da40",
+    "url": "/react-click/static/css/main.80dd4c88.chunk.css"
+  },
+  {
+    "revision": "ba449005ced7463ae89d",
+    "url": "/react-click/static/js/2.44098778.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-click/static/js/2.44098778.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcbeff9d80694e17da40",
+    "url": "/react-click/static/js/main.f41d495e.chunk.js"
+  },
+  {
+    "revision": "e4d651fc2d1d7b458a56",
+    "url": "/react-click/static/js/runtime-main.b3fa370d.js"
+  }
+]);
