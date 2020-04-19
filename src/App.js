22e594e5644caa12d50980d6f411faf6206910fe
xiongkb <mkan-bx@hotmail.com>
2020-04-18 18:18:15 -0700

    fixed shuffle and added different images

--- src/App.js
+++ src/App.js
@@ -5,10 +5,17 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
+    const imgNums = [];
+
+    for (let i = 1; i < 71; i++) {
+      imgNums.push(i)
+    };
+    console.log(imgNums);
+
     this.state = {
       score: 0,
       clicked: [],
-      imgNums: [237, 238, 123, 125, 132, 144]
+      imgNums: imgNums
     }
   }
 
@@ -21,18 +28,21 @@ class App extends React.Component {
       this.setState({ score: 0 });
       alert("Too bad!")
     }
+    console.log("You clicked on:")
     console.log(this.state.clicked)
+    console.log("--------")
+
   }
 
   render() {
     const ranImgNums = [];
 
-    while (ranImgNums.length < 6) {
-      const ranNum = this.state.imgNums[Math.floor(Math.random() * 6)];
+    while (ranImgNums.length < 8) {
+      const ranNum = this.state.imgNums[Math.floor(Math.random() * 69)];
       if (!ranImgNums.includes(ranNum)) {
         ranImgNums.push(ranNum);
       }
-      console.log(ranImgNums)
+      
     }
 
     return (
@@ -41,12 +51,19 @@ class App extends React.Component {
         <p>Click on the images no more than once! Test your memory skills!</p>
 
         <div>
-          <img alt="random Img" src={`https://picsum.photos/id/${ranImgNums[0]}/200/`} onClick={() => this.clicker(1)}></img>
-          <img alt="random Img" src={`https://picsum.photos/id/${ranImgNums[1]}/200/`} onClick={() => this.clicker(2)}></img>
-          <img alt="random Img" src={`https://picsum.photos/id/${ranImgNums[2]}/200/`} onClick={() => this.clicker(3)}></img><br></br>
-          <img alt="random Img" src={`https://picsum.photos/id/${ranImgNums[3]}/200/`} onClick={() => this.clicker(4)}></img>
-          <img alt="random Img" src={`https://picsum.photos/id/${ranImgNums[4]}/200/`} onClick={() => this.clicker(5)}></img>
-          <img alt="random Img" src={`https://picsum.photos/id/${ranImgNums[5]}/200/`} onClick={() => this.clicker(6)}></img>
+          <img alt="random Img" src={`https://i.pravatar.cc/200?img=${ranImgNums[0]}`} onClick={() => this.clicker(ranImgNums[0])}></img>
+          <img alt="random Img" src={`https://i.pravatar.cc/200?img=${ranImgNums[1]}`} onClick={() => this.clicker(ranImgNums[1])}></img>
+          <img alt="random Img" src={`https://i.pravatar.cc/200?img=${ranImgNums[2]}`} onClick={() => this.clicker(ranImgNums[2])}></img><br></br>
+          <img alt="random Img" src={`https://i.pravatar.cc/200?img=${ranImgNums[3]}`} onClick={() => this.clicker(ranImgNums[3])}></img>
+          <img alt="random Img" src={`https://i.pravatar.cc/200?img=${ranImgNums[4]}`} onClick={() => this.clicker(ranImgNums[4])}></img>
+          <img alt="random Img" src={`https://i.pravatar.cc/200?img=${ranImgNums[5]}`} onClick={() => this.clicker(ranImgNums[5])}></img><br></br>
+          <img alt="random Img" src={`https://i.pravatar.cc/200?img=${ranImgNums[6]}`} onClick={() => this.clicker(ranImgNums[6])}></img>
+          <img alt="random Img" src={`https://i.pravatar.cc/200?img=${ranImgNums[7]}`} onClick={() => this.clicker(ranImgNums[7])}></img>
+          <img alt="random Img" src={`https://i.pravatar.cc/200?img=${ranImgNums[8]}`} onClick={() => this.clicker(ranImgNums[8])}></img><br></br>
+        </div>
+
+        <div>
+          <h4>Your Score: {this.state.score}</h4>
         </div>
       </div>
     );
